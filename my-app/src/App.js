42e45ad05bf796f5f975eff847e01a74f07007b2
Xassha David <xasshadavid@protonmail.com>
2022-10-02 15:17:06 +1300

    images for website

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from './components/NavBar'
 import Hero from './components/Hero';
+import Analytics from './components/Analytics';
 
 function App() {
   return (
     <div>
       <NavBar />
       <Hero />
+      <Analytics />
     </div>
   );
 }
