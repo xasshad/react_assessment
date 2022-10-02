eb7e362b845a9da209b26219333184742c2f7710
Xassha David <xasshadavid@protonmail.com>
2022-10-02 15:29:40 +1300

    commit

--- my-app/src/components/Analytics.jsx
+++ my-app/src/components/Analytics.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Analytics = () => {
   return (
-    <div>
-        Analytics
+    <div className="w-full bg-white py-16 px-4">
+        <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
+            
+        </div>
+        
     </div>
   )
 }
