2cc946609d24df1b809ed6cc5553c9430d179686
Xassha David <xasshadavid@protonmail.com>
2022-10-10 20:42:37 +1300

    update

--- my-app/src/components/NavBar.jsx
+++ my-app/src/components/NavBar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
       </div>
       
       <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>DATABLAST.</h1>
           <li className='p-4 border-b border-gray-600'><Link to="/">Home</Link></li>
           <li className='p-4 border-b border-gray-600'><Link to="/resources">Resources</Link></li>
           <li className='p-4 border-b border-gray-600'><Link to="/about">About</Link></li>
